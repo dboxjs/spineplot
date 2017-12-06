4d5b235b7ffc8d70bc106421f0fd47b4da33d1f2
Alecs Garza <alecsgarza@Alecss-iMac.local>
2017-12-06 15:13:15 -0600

    wip

--- bars.js
+++ bars.js
@@ -46,11 +46,6 @@ export default function(config) {
     return vm;
   }
 
-  Bars.prototype.end = function() {
-    var vm = this;
-    return vm._chart;
-  }
-
   Bars.prototype.colorScale = function(colorScale) {
     var vm = this;
     if(Array.isArray(colorScale)) {
@@ -71,6 +66,11 @@ export default function(config) {
     return vm;
   }
 
+  Bars.prototype.end = function() {
+    var vm = this;
+    return vm._chart;
+  }
+
   //-------------------------------
   //Triggered by the chart.js;
   Bars.prototype.chart = function(chart) {
@@ -81,6 +81,12 @@ export default function(config) {
 
   Bars.prototype.data = function(data) {
     var vm = this;
+    
+    //In case we want to filter observations
+    if(vm._config.data.filter){
+      data = data.filter(vm._config.data.filter);
+    }
+  
     vm._data = data.map(function(d) {
       if(d[vm._config.x] == Number(d[vm._config.x]))
         d[vm._config.x] = +d[vm._config.x];
@@ -88,6 +94,12 @@ export default function(config) {
         d[vm._config.y] = +d[vm._config.y];
       return d;
     });
+
+    if(vm._config.bars.quantiles){
+      vm._quantiles = vm._setQuantile(data);
+      vm._minMax = d3.extent(data, function(d) { return +d[vm._config.color]; })
+    }
+      
     return vm;
   }
 
@@ -123,6 +135,9 @@ export default function(config) {
 
     if(!vm._scales.color)
       vm._scales.color = d3.scaleOrdinal(vm._config.colorScale);
+
+    if(vm._config.bars.quantiles)
+      vm._scales.color = vm._getQuantileColor;
     return vm;
   }
 
@@ -188,7 +203,8 @@ export default function(config) {
           .attr("y", function(d) { return vm._scales.y(d[vm._config.y]);})
           .attr("width", function(d){ return vm._scales.x.bandwidth ? vm._scales.x.bandwidth() : vm._scales.x(d[vm._config.x]) })
           .attr("height", function(d) { return vm._chart._height - vm._scales.y(d[vm._config.y]); })
-          .attr("fill", function(d){ return vm._scales.color(d[vm._config.color])})
+          .attr("fill", function(d){ 
+            return vm._scales.color(d[vm._config.color])})
           .style("opacity", 0.9)
           .on('mouseover', function(d) {
             vm._tip.show(d, d3.select(this).node());
@@ -200,6 +216,105 @@ export default function(config) {
 
           });
     return vm;
+  }
+
+  Bars.prototype._setQuantile = function(data){
+    var vm = this; 
+    var values = [];
+    var quantile = []; 
+
+    if(vm._config.bars.quantiles &&  vm._config.bars.quantiles.predefinedQuantiles 
+        && vm._config.bars.quantiles.predefinedQuantiles.length > 0){
+      return vm._config.bars.quantiles.predefinedQuantiles;
+    }
+
+    data.forEach(function(d){      
+      values.push(+d[vm._config.z]);
+    });
+
+    values.sort(d3.ascending);
+    
+    //@TODO use quantile scale instead of manual calculations 
+    if(vm._config && vm._config.bars && vm._config.bars.quantiles && vm._config.bars.quantiles.buckets){
+
+      if(vm._config.bars.quantiles.ignoreZeros === true){
+        var aux = _.dropWhile(values, function(o) { return o <= 0 });
+        quantile.push(values[0]);
+        quantile.push(0);
+        
+        for(var i = 1; i <= vm._config.bars.quantiles.buckets - 1; i++ ){        
+          quantile.push( d3.quantile(aux,  i* 1/(vm._config.bars.quantiles.buckets - 1) ) )
+        }
+
+      }else{
+        quantile.push( d3.quantile(values, 0) )
+        for(var i = 1; i <= vm._config.bars.quantiles.buckets; i++ ){        
+          quantile.push( d3.quantile(values,  i* 1/vm._config.bars.quantiles.buckets ) )
+        }
+      }
+        
+    }else{
+      quantile = [ d3.quantile(values, 0), d3.quantile(values, 0.2), d3.quantile(values, 0.4), d3.quantile(values, 0.6), d3.quantile(values, 0.8), d3.quantile(values,1) ];
+    }
+   
+    //@TODO - VALIDATE WHEN ZEROS NEED TO BE PUT ON QUANTILE 1 AND RECALCULATE NON ZERO VALUES INTO THE REST OF THE BUCKETS
+    if( vm._config.bars.quantiles && vm._config.bars.quantiles.buckets 
+        && vm._config.bars.quantiles.buckets === 5){
+
+      if( quantile[1] === quantile[2] && quantile[2] === quantile[3] && quantile[3] === quantile[4] && quantile[4] === quantile[5]){
+        quantile = [ d3.quantile(values, 0), d3.quantile(values, 0.2) ];
+      }
+    }
+   
+    return quantile;
+  }
+
+  Bars.prototype._getQuantileColor = function(d){
+    var vm = this; 
+    var total = parseFloat(d);
+
+    //@TODO use quantile scale instead of manual calculations 
+    if(vm._config && vm._config.bars && vm._config.bars.quantiles && vm._config.bars.quantiles.colors){
+      if(vm._quantiles.length > 2){
+
+        if(vm._config && vm._config.bars && vm._config.bars.min !== undefined && vm._config.bars.max !== undefined){
+          if(total < vm._config.bars.min || total > vm._config.bars.max){
+            console.log('outOfRangeColor', total, vm._config.bars.min ,vm._config.bars.max)
+            return vm._config.bars.quantiles.outOfRangeColor; 
+          }
+        }else{
+          if(total < vm.minMax[0] || total > vm.minMax[1]){
+            console.log('outOfRangeColor', total, vm._config.bars.min ,vm._config.bars.max)
+            return vm._config.bars.quantiles.outOfRangeColor; 
+          }
+        }
+
+        if(total <= vm._quantiles[1]){
+          return vm._config.bars.quantiles.colors[0];//"#f7c7c5";
+        }else if(total <= vm._quantiles[2]){
+          return vm._config.bars.quantiles.colors[1];//"#e65158";
+        }else if(total <= vm._quantiles[3]){
+          return vm._config.bars.quantiles.colors[2];//"#c20216";
+        }else if(total <= vm._quantiles[4]){
+          return vm._config.bars.quantiles.colors[3];//"#750000";
+        }else if(total <= vm._quantiles[5]){
+          return vm._config.bars.quantiles.colors[4];//"#480000";
+        }
+
+      }
+    }
+
+    if(vm._quantiles.length == 2){
+      /*if(total === 0 ){
+        return d4theme.colors.quantiles[0];//return '#fff';
+      }else if(total <= vm._quantiles[1]){
+        return d4theme.colors.quantiles[1];//return "#f7c7c5";
+      }*/
+      if(total <= vm._quantiles[1]){
+        return vm._config.bars.quantiles.colors[0];//"#f7c7c5";
+      }
+    }
+
   }
 
   return new Bars(config);
